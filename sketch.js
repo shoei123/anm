32f6431d11ed6722c55307c4aa2066c1e6a473e3
shoei123 <80819233+shoei123@users.noreply.github.com>
2021-06-08 15:31:57 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://hkr005078.github.io/imageai/';
+let imageModelURL = 'https://shoei123.github.io/anm/';
 
 // Video
 let video;
